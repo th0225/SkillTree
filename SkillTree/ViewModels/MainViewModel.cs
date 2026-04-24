04846159f52f6d763b0c1a03c2cb327d5a586e8a
TerryHung <excalibur0225@gmail.com>
2026-04-24 09:19:17 +0800

    feature: 加入viewmodel

--- SkillTree/ViewModels/MainViewModel.cs
+++ SkillTree/ViewModels/MainViewModel.cs
@@ -1,9 +1,50 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Linq;
+using SkillTree.Models;
+using SkillTree.Services;
 
 namespace SkillTree.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+    private readonly SkillTreeService _service = new();
+    private readonly SkillTreeData _currentTree;
+
+    public ObservableCollection<SkillNodeViewModel> Nodes { get; } = [];
+
     [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    private SkillNodeViewModel? _selectedNode;
+
+    public MainViewModel()
+    {
+        _currentTree = _service.CreateSampleTree();
+        LoadNodes();
+    }
+
+    private void LoadNodes()
+    {
+        Nodes.Clear();
+        foreach (var node in _currentTree.Nodes)
+        {
+            var vm = new SkillNodeViewModel(node);
+            vm.SyncFromModel();
+            Nodes.Add(vm);
+        }
+    }
+
+    [RelayCommand]
+    private void ToggleNode(SkillNodeViewModel nodeVm)
+    {
+        var model = _currentTree.Nodes.First(n => n.Id == nodeVm.Id);
+
+        if (model.Status == SkillStatus.Unlocked)
+            _service.TryLock(model, _currentTree);
+        else
+            _service.TryUnlock(model, _currentTree);
+        
+        foreach (var vm in Nodes)
+            vm.SyncFromModel();
+    }
 }
---
+++ SkillTree/ViewModels/SkillNodeViewModel.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SkillTree.Models;
+
+namespace SkillTree.ViewModels;
+
+public partial class SkillNodeViewModel(SkillNode model) : ObservableObject
+{
+    private readonly SkillNode _model = model;
+
+    public string Id => _model.Id;
+    public string Name => _model.Name;
+    public string Description => _model.Description;
+    public SkillCategory Category => _model.Category;
+    public double X => _model.X;
+    public double Y => _model.Y;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsUnlocked))]
+    [NotifyPropertyChangedFor(nameof(IsAvailable))]
+    [NotifyPropertyChangedFor(nameof(IsLocked))]
+    private SkillStatus _status = SkillStatus.Locked;
+
+    public bool IsUnlocked => Status == SkillStatus.Unlocked;
+    public bool IsAvailable => Status == SkillStatus.Available;
+    public bool IsLocked => Status == SkillStatus.Locked;
+
+    public void SyncFromModel()
+    {
+        Status = _model.Status;
+    }
+}
